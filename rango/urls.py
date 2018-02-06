b3abaaaddf44433116b06c81caf9fe471a673852
Sam <2261522g@student.gla.ac.uk>
2018-02-06 20:04:14 +0000

    end of chapter 8

--- rango/urls.py
+++ rango/urls.py
@@ -3,6 +3,7 @@ from rango import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+#app_name = 'rango'
 urlpatterns = [
         url(r'^$', views.index, name='index'),
 	url(r'^index/$',views.index, name = 'index'),
