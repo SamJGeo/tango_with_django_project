207f3ce00908f52269168ea3a2f9c5e644889173
Sam <2261522g@student.gla.ac.uk>
2018-02-08 14:20:08 +0000

    end of chapter 10

--- rango/views.py
+++ rango/views.py
@@ -4,20 +4,37 @@ from django.http import HttpResponseRedirect,HttpResponse
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.core.urlresolvers import reverse
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
+from datetime import datetime
+
 
 
 def index(request):
+    request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list,'pages':page_list}
+
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    response = render(request, 'rango/index.html', context=context_dict)
     
-    return render(request,'rango/index.html',context_dict)
+    return response
 
 
 def about(request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
     context_dict = {}
-    return render(request,'rango/about.html',context=context_dict)
+
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    
+    
+    response = render(request, 'rango/about.html', context_dict)
+        
+    return response
 
 
 def show_category(request, category_name_slug):
@@ -108,7 +125,8 @@ def register(request):
         user_form = UserForm()
         profile_form = UserProfileForm()
 
-    return render(request,'rango/register.html',{'user_form': user_form,'profile_form': profile_form,'registered': registered})
+    return render(request,'rango/register.html',{'user_form': user_form,'profile_form': profile_form,'registered': registered})
+
 
 def user_login(request):
 
@@ -130,11 +148,13 @@ def user_login(request):
             return HttpResponse("Invalid login details supplied.")
 
     else:
-        return render(request, 'rango/login.html', {})
+        return render(request, 'rango/login.html', {})
+
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return HttpResponse("Since you're logged in, you can see this text!")
+
 
 #must be logged in to log out
 @login_required
@@ -142,4 +162,27 @@ def user_logout(request):
     
     logout(request)
     #return to homepage
-    return HttpResponseRedirect(reverse('index'))
+    return HttpResponseRedirect(reverse('index'))
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,'last_visit',
+    str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+    
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        visits = 1
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
+
